d00c8530e64ae797932e6d604f9953c77670ba00
oaslan <oaslan@innova.com.tr>
2022-12-06 15:17:50 +0300

    catalog service implemented.

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -1,3 +1,7 @@
+using Catalog.API.Data.Interfaces;
+using Catalog.API.Data;
+using Catalog.API.Repositories.Interfaces;
+using Catalog.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +29,9 @@ namespace Catalog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddScoped<ICatalogContext, CatalogContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
